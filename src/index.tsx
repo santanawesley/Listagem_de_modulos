35d81c2953a66ee4d96bf51e6edba5aef3b76226
Wesley Santana <wesley.santana@opah.com.br>
2024-05-02 13:34:16 -0300

    Configuring tailwind usage

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import App from "./app";
+import "./styles/globals.css";
 
 import { createRoot } from "react-dom/client";
 const container = document.getElementById("root");
