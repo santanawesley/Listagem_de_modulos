a171cf186d87e5f4e1a31c23b399f7adf3801422
Wesley Santana <wesley.santana@opah.com.br>
2024-05-03 00:52:52 -0300

    Adding error message

--- src/app.tsx
+++ src/app.tsx
@@ -14,6 +14,7 @@ const App: React.FC = () => {
     {} as ModulesActive
   );
   const [subModules, setSubModules] = useState<SubModule[]>([]);
+  const [errorDataModules, setErrorDataModules] = useState<Boolean>(false);
 
   useEffect(() => {
     fetchModules();
@@ -31,6 +32,7 @@ const App: React.FC = () => {
       setSubModules(body[0].subModulos);
     } catch (error) {
       console.error("Erro ao buscar os módulos:", error);
+      setErrorDataModules(true);
     }
   };
 
@@ -42,43 +44,53 @@ const App: React.FC = () => {
   return (
     <div className="bg-slate-100 min-h-screen flex flex-col text-zinc-600">
       <Header />
-      <div className="flex grow gap-6 px-5">
-        <nav>
-          <p className="text-2xl pt-7 pr-1 pb-5 pl-0">Módulos:</p>
-          <ul className="gap-6 flex flex-col">
-            {modules.map((module) => (
-              <li key={module.id} className="w-56 cursor-pointer">
-                <a
-                  onClick={() => selectModules(module)}
-                  className={`rounded py-2 px-4 flex ${
-                    module.id === modulesActive.id
-                      ? "bg-lime-600 text-white"
-                      : "bg-lime-400 text-zinc-600"
-                  }`}
+      {errorDataModules ? (
+        <div className="flex grow justify-center">
+          <p className="pt-24 font-bold text-center text-xl">
+            Não foi possível acessar as informações de Módulos e Submódulos no
+            momento. <br />
+            Tente mais tarde ou entre em contato conosco.
+          </p>
+        </div>
+      ) : (
+        <div className="flex grow gap-6 px-5">
+          <nav>
+            <p className="text-2xl pt-7 pr-1 pb-5 pl-0">Módulos:</p>
+            <ul className="gap-6 flex flex-col">
+              {modules.map((module) => (
+                <li key={module.id} className="w-56 cursor-pointer">
+                  <a
+                    onClick={() => selectModules(module)}
+                    className={`rounded py-2 px-4 flex ${
+                      module.id === modulesActive.id
+                        ? "bg-lime-600 text-white"
+                        : "bg-lime-400 text-zinc-600"
+                    }`}
+                  >
+                    <strong>{module.Descricao}</strong>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <main className="grow">
+            <h2 className="text-center p-6 text-4xl font-medium mb-4 text-zinc-600">
+              Submódulos de{" "}
+              <span className="text-lime-600">{modulesActive.Descricao}</span>
+            </h2>
+            <ul className="flex flex-wrap gap-3 gap-y-9 justify-center">
+              {subModules.map((submodule) => (
+                <li
+                  key={submodule.id}
+                  className="flex justify-center items-center text-center shadow bg-gradient-to-tr from-green-500 to-white w-52 p-8 rounded"
                 >
-                  <strong>{module.Descricao}</strong>
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <main className="grow">
-          <h2 className="text-center p-6 text-4xl font-medium mb-4 text-zinc-600">
-            Submódulos de{" "}
-            <span className="text-lime-600">{modulesActive.Descricao}</span>
-          </h2>
-          <ul className="flex flex-wrap gap-3 gap-y-9 justify-center">
-            {subModules.map((submodule) => (
-              <li
-                key={submodule.id}
-                className="flex justify-center items-center text-center shadow bg-gradient-to-tr from-green-500 to-white w-52 p-8 rounded"
-              >
-                {submodule.Descricao}
-              </li>
-            ))}
-          </ul>
-        </main>
-      </div>
+                  {submodule.Descricao}
+                </li>
+              ))}
+            </ul>
+          </main>
+        </div>
+      )}
       <Footer />
     </div>
   );
